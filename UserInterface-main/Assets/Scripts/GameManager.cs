31c4719755e0ce37e3ae79c4cafdc823496dafbe
SumRandomPerson <mikegui343@gmail.com>
2024-05-28 06:35:35 -0400

    Initial commit

---
+++ UserInterface-main/Assets/Scripts/GameManager.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
+    public bool isGameActive;
+
+    public GameObject titleScreen;
+
+    public Button restartButton;
+
+    [Min(0)] public int lives;
+
+    private float _spawnRate = 1.0f;
+    private int _score;
+
+    private AudioSource _backgroundMusic;
+
+    public Slider volumeSlider;
+
+    public GameObject pausedImage;
+
+    public bool isPaused = false;
+
+    public GameObject trailMaker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _backgroundMusic = GetComponent<AudioSource>();
+        UpdateVolume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGameActive)
+        {
+            TogglePaused();
+        }
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            Instantiate(targets[Random.Range(0, targets.Count)]);
+        }
+    }
+
+    public void UpdateScore(int addToScore)
+    {
+        _score += addToScore;
+        scoreText.text = "Score: " + _score;
+    }
+
+    public void UpdateLives()
+    {
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void UpdateVolume()
+    {
+        _backgroundMusic.volume = volumeSlider.value;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        trailMaker.gameObject.SetActive(false);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(float difficulty)
+    {
+        isGameActive = true;
+        _score = 0;
+        _spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives();
+
+        titleScreen.gameObject.SetActive(false);
+        trailMaker.gameObject.SetActive(true);
+    }
+
+    public void DecreaseLives()
+    {
+        if (lives > 0)
+        {
+            lives -= 1;
+            UpdateLives();
+        }
+    }
+
+    void TogglePaused()
+    {
+        isPaused = !isPaused;
+        pausedImage.SetActive(isPaused);
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            trailMaker.gameObject.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            trailMaker.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ UserInterface-main/Assets/Scripts/Target.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody _targetRb;
+    private float _minSpeed = 12.0f;
+    private float _maxSpeed = 16.0f;
+    private float _maxTorque = 10.0f;
+    private float _xRange = 4.0f;
+    private float _ySpawnPos = -2.0f;
+
+    private GameManager _gameManager;
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetRb = GetComponent<Rigidbody>();
+
+        _targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        _targetRb.AddTorque(RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (_gameManager.isGameActive && _gameManager.isPaused == false && Input.GetMouseButton(0))
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, gameObject.transform.rotation);
+            _gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if (gameObject.CompareTag("Good") && _gameManager.lives > 0)
+        {
+            _gameManager.DecreaseLives();
+        }
+
+        if (_gameManager.lives == 0)
+        {
+            _gameManager.GameOver();
+        }
+    }
+
+    private Vector3 RandomForce() => Vector3.up * Random.Range(_minSpeed, _maxSpeed);
+
+    private Vector3 RandomTorque() => new(Random.Range(-_maxTorque, _maxTorque),
+                                          Random.Range(-_maxTorque, _maxTorque),
+                                          Random.Range(-_maxTorque, _maxTorque));
+
+    private Vector3 RandomSpawnPos() => new(Random.Range(-_xRange, _xRange), _ySpawnPos);
+}
